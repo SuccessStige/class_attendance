dd31026050574e8496d17bbdc54dc89d4345ebe7
SuckyStige <stigehub@gmail.com>
2026-02-21 17:04:50 +0100

    Pushing Requirement File

--- School_attendance/settings.py
+++ School_attendance/settings.py
@@ -85,14 +85,14 @@ WSGI_APPLICATION = 'School_attendance.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'attendance_register',
-        'USER': 'attendance_register',
-        'PASSWORD': 'attendance_password',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'attendance_register',
+        # 'USER': 'attendance_register',
+        # 'PASSWORD': 'attendance_password',
+        # 'HOST': 'localhost',
+        # 'PORT': '3306',
     }
 }
 
